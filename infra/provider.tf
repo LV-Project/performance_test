08b0412344dd13e6a4de71ac40b9bfdec040b0d6
Leonardo Villalba <adg.leo.lv@gmail.com>
2023-03-08 12:31:32 -0500

    ci workflow created

--- infra/provider.tf
+++ infra/provider.tf
@@ -15,5 +15,5 @@ provider "aws" {
 locals {
   key_name         = "leo_master"
   ssh_user         = "ubuntu"
-  private_key_path = "/home/lv-project/personalProjects/PTCI/infra/leo_master.pem"
+  private_key_path = "/home/lv-project/personalProjects/PTCI/performance_test/infra/leo_master.pem"
 }
